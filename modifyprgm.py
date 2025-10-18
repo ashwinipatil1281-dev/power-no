fdb81390f1ccffe7e8604cbf5445c2849975e942
ashwinipatil1281@gmail.com <ashwinipatil1281-dev>
2025-10-18 12:53:07 +0530

    save

---
+++ modifyprgm.py
@@ -0,0 +1,11 @@
+pow=(input("enter a base number:"));
+exp=(input("enter a exponent number:"));
+
+result=(pow)**(exp)
+
+print("power of number:",{result});
+
+if (base==0 and exponent<0):
+    print("cannot perform when base is zero")
+else:
+    print("power of number:",{result});
